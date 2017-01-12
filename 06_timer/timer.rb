38f62a88a6a41e019b007ee6cf3e6f4618238490
ToTenMilan <grzegorz.od.tych.milanowskich@gmail.com>
2017-01-12 11:15:13 +0000

    Do ex. 06_timer and Finish learn_ruby

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+  
+  attr_reader :seconds
+  
+  def initialize
+    @seconds = 0
+  end
+  
+  def seconds=(seconds)
+    @seconds = Time.at(seconds).strftime("%H:%M:%S")
+  end
+  
+  def time_string
+    @seconds
+  end
+  
+  def padded(seconds)
+    seconds < 10 ? "0" + seconds.to_s : seconds.to_s
+    # if seconds < 10
+    #   "0" + seconds.to_s
+    # else
+    #   seconds.to_s
+    # end
+  end
 end
